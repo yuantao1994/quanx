80e4854d446441107e49ce3fd8f02c2ac55a5ec8
wukuaiqian <1132500764@qq.com>
2021-07-13 20:43:14 +0800

    temp

---
+++ 50off/num2.js
@@ -0,0 +1,5 @@
+
+
+const $ = new Env('京东汽车');
+$.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
+ 
